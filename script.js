a27a830fb23bfec31b2bd910fb1ecad3c6a986e1
Янина <79905479+yaninasu551@users.noreply.github.com>
2024-04-04 23:42:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,149 @@
-// script.js
-function findGifts() {
-  var age = document.getElementById("age").value;
-  var recipient = document.getElementById("recipient").value;
-  var occasion = document.getElementById("occasion").value;
-  var interests = document.getElementById("interests").value;
-
-  var giftListElement = document.getElementById("giftList");
-  giftListElement.innerHTML = ""; // Очищаем список подарков
-
-  // Получаем подарки на основе выбранных реквизитов
-  var gifts = getGifts(age, recipient, occasion, interests);
-
-  // Добавляем подарки в список
-  gifts.forEach(function(gift) {
-      var li = document.createElement("li");
-      li.textContent = gift;
-      giftListElement.appendChild(li);
-  });
+let gifts_list = {
+    age: {
+        "0-10": {
+          "Игрушка": "https://www.funtastik.by/upload/iblock/9c3/9c30256df75fb2af12d073710d29fe36.jpg",
+          "Книжка": "https://images.deal.by/129603601_w600_h600_129603601.jpg",
+          "Конструктор": "https://7745.by/img/imagecache/card_medium_image_for_product/uploads/zoomos/img/972215_0.jpg"
+        },
+        "11-20": {
+          "Спортивный инвентарь": "https://img.freepik.com/premium-vector/sports-equipment_38359-143.jpg",
+          "Модная одежда": "https://i.pinimg.com/564x/6a/07/54/6a075452394bc8a8162f183e4f9153a4.jpg",
+          "Гаджет": "https://gb.ru/blog/wp-content/uploads/2023/06/shutterstock_1875797689.jpg.webp"
+        },
+        "21-30": {
+          "Книга": "https://lh4.googleusercontent.com/proxy/nXVrvMEYIdI2BuTh_sFOz4mHrlh79gS8qgfnAu2z_stn4fkJci_wVlsnwaCCIMd0ES0irU4VM0fbnvA6ucJnR4UfIZd-D5h66Jw",
+          "Билеты на концерт": "https://images.aif.by/007/645/88e7a718a3d573f59e1d0b29a6a9c6fd.jpg",
+          "Дизайнерская одежда": "https://i.pinimg.com/564x/5f/e4/2f/5fe42fa2084b11d8069d98a804c0d6de.jpg"
+        },
+        "31-40": {
+          "Кухонная утварь": "https://images.deal.by/410105591_w600_h600_410105591.jpg",
+          "Спа-подарочный набор": "https://cs2.livemaster.ru/storage/7a/24/8535964d0399c47838ddb2d3cazd--suveniry-i-podarki-podarochnyj-nabor-beauty-and-spa.jpg",
+          "Аксессуары для отдыха": ""
+        },
+        "41-50": {
+          "Ароматическая свеча": "https://present-simple.by/wp-content/uploads/2021/06/5-min.png",
+          "Товары для сада": "https://instrumental.by/ru/files/news/image/0/0/1608019851.jpg",
+          "Ювелирные украшения": "https://rg.ru/uploads/images/179/43/88/Zahod-Jewelry2.jpg"
+        },
+        "51+": {
+          "Подписка на журнал": "https://yuniline.by/img/products/pr_komplekt-neposeda-3-9-tsarstvo.png",
+          "Комфортный халат": "https://www.ruskniga.com/media/catalog/product/cache/5e44578c7bcfaf8897aa535acf055f26/r/s/rst_6263_copy.jpg__1.jpg",
+          "Подарочный сертификат": "https://cheesecard.by/wp-content/uploads/2021/08/photo_2021-08-01_16-27-29.jpg"
+        }
+      },
+        recipient: {
+        "Мужчины": {
+          "Устройство для барбекю": "https://ae04.alicdn.com/kf/Aa1c71d145cae4a15a955d20a9a81c74dm.jpg",
+          "Инструменты": "https://dsnspb.ru/uploads/2019_03/0006-003-.png",
+          "Эксклюзивный карманный нож": "https://rezat.ru/db.img/catalog/gallery/wm/whb10-1_15_.jpg"
+        },
+        "Женщины": {
+          "Косметический набор": "https://catalog.detmir.st/media/d5cZsHo5oq0hpsQp02q76OrFDXk--pojU3L0r40JRLk=?preset=site_product_gallery_r1500",
+          "Украшения": "https://www.shoppingschool.ru/netcat_files/userfiles/Articles/2018/YKRASHENIE/1.jpg",
+          "Спа-услуги": "https://sochiparko.ru/wp-content/uploads/price-spa-1.jpg"
+        },
+        "Дети": {
+          "Игрушка": "https://ir.ozone.ru/s3/multimedia-1/c1000/6533735893.jpg",
+          "Спортивный инвентарь": "https://ros-test.info/images/article/5b0d31a35039b.jpg",
+          "Книжка": "https://azbookvarik.ru/upload/iblock/712/712353e75cb4fa49d771497c69e9e245.jpg"
+        },
+        "Пары": {
+          "Романтический ужин": "https://www.russianireland.com/wp-content/uploads/2022/02/%D1%80%D0%BE%D0%BC%D0%B0%D0%BD%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D1%83%D0%B6%D0%B8%D0%BD-%D0%BD%D0%B0-%D0%BF%D0%B0%D1%80%D1%8B.jpg",
+          "Совместная поездка": "https://c.tenor.com/Y6oK7_XEtygAAAAC/tenor.gif",
+          "Подарочный сертификат на спа": "https://spa-di.ru/upload/iblock/0c6/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_03.jpg"
+        },
+        "Друзья": {
+          "Смешные носки": "https://malevich.evo.run/img?url=https://images.crafta.ua/products/3365414&w=330&h=330",
+          "Игра настольная": "https://images.firma-gamma.ru/images/0/2/g27800012622p.jpg",
+          "Абонемент в спортзал": "https://avatars.mds.yandex.net/get-mpic/7519991/img_id6298728417050657069.jpeg/orig"
+        }
+      },
+        occasion: {
+        "День рождения": {
+          "Праздничный торт": "https://renty.ua/wp-content/uploads/2019/03/44-cover_big.jpg",
+          "Праздничные шары": "https://daflor.by/upload/iblock/8b1/8b17bd39608453dafbe61f4824a2ef57.jpg",
+          "Сюрприз-подарок": "https://img.freepik.com/premium-vector/surprise-boxes-opened-gift-cardboard-containers-with-explode-confetti-ribbons-decent-vector-realistic-template-illustration-gift-surprise-birthday-celebration-explosion-confetti_80590-11883.jpg"
+        },
+        "Юбилей": {
+          "Подарок с символикой юбилея": "https://futbokraina.com.ua/image/cache/new/catalog/GS/chashka-classic/red-chashka-53723.jpg",
+          "Эксклюзивный подарок": "https://cs2.livemaster.ru/storage/7f/ac/16c70ce25751b52b7c40c2467ets--suveniry-i-podarki-eksklyuzivnyj-podarok-lampovye-chasy-nasto.jpg",
+          "Путешествие": "https://www.myjane.ru/data/cache/2014jun/25/04/238434_13212-670x400x.jpg"
+        },
+        "Свадьба": {
+          "Сервиз для чая": "https://www.teashop.by/wp-content/uploads/chajnyj-serviz-keramicheskij-1.jpg",
+          "Коврик для ванной": "https://vdom.by/upload/iblock/a58/bklbwy4zidielsbk36m09mdqib3yxly4/103649.jpg",
+          "Подарочные деньги": "https://images.deal.by/320898794_w600_h600_320898794.jpg"
+        },
+        "Новый год": {
+          "Набор для горячего шоколада": "https://ligatura.by/image/catalog/products/48756_0.jpg",
+          "Новогодние украшения": "https://www.terrakot-furn.ru/images/categories/5032.jpg",
+          "Подарочная книга": "https://dubna.ru/sites/default/files/articles/2021/10/05/14854.jpg"
+        },
+        "Другой": {
+          "Персонализированный подарок": "https://superpupers.com/image/cache/catalog/katalog/superpupers/personal/termochashka-vashdizajn-228x228.jpg",
+          "Нечто необычное": "https://i.pinimg.com/564x/48/b4/1f/48b41fac05dd0fd7b981316cb5b14435.jpg",
+          "Уникальный предмет интерьера": "https://s.zefirka.net/images/2022-05-25/somnitelnye-i-strannye-veshhi-ot-genialnyx-dizajnerov/somnitelnye-i-strannye-veshhi-ot-genialnyx-dizajnerov-1.jpg"
+        },
+      },
+        interests: {
+        "Книги": {
+          "Лучший бестселлер года": "https://images.unian.net/photos/2020_03/thumb_files/400_0_1583961951-9852.jpg?0.08298298363106404",
+          "Классическая книга": "https://s5-goods.ozstatic.by/2000/568/37/1/1037568_0.jpg",
+          "Аудиокнига": "https://assets.gq.ru/photos/5e78d2bd8c8d8a0008a2e5bf/16:9/w_2560%2Cc_limit/0.jpg"
+        },
+        "Спорт": {
+          "Набор для йоги": "https://cdn1.ozone.ru/s3/multimedia-4/6373573012.jpg",
+          "Спортивный инвентарь": "https://fabricators.ru/sites/default/files/styles/news/public/img_article/sport-1.jpg?itok=ep5n6nxt",
+          "Абонемент в фитнес-клуб": "https://lh3.googleusercontent.com/D9MlJ8823FQg_EiM1YjAkCY0pmKhgoVDRIhWJQRJa2XFfFlMnZT376XOfA248Mq6vev_sYOOUXcuYECL_ObWVAZm6cpwVUpxvX4gAXNhD4PDKOe4G7igit12tm4m_yLNUkUlWXqHD08To4PBke9uUig"
+        },
+        "Технологии": {
+          "Умные часы": "https://xistore.by/upload/resize/element/73414/16f/f5f03880d8459735429784a9dc7b7845_482_482_80.jpg",
+          "Наушники": "https://ir.ozone.ru/s3/multimedia-8/c1000/6164807864.jpg",
+          "Гаджет последней модели": "https://mobile-review.com/all/wp-content/uploads/2022/04/1-1.jpg"
+        },
+        "Путешествия": {
+          "Компактный чемодан": "https://autotuning-bmw.ru/r/800x600/static/1/kompaktnyiy-chemodan-mini-cabin-trolley-bmw-80222445678.jpg",
+          "Путевой набор": "https://vikowest.by/upload/iblock/449/nbcrhohkx27tbh3md8m0rsrzwehioryl.jpg",
+          "Путеводитель": "https://portal-kultura.ru/upload/iblock/541/%D0%BF%D1%83%D1%82%D0%B5%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D0%B8%20(1).jpg"
+        },
+        "Мода": {
+          "Аксессуары": "https://estellemoda.ru/wp-content/uploads/2022/04/accessoriesforbeginners.jpg",
+          "Модная одежда": "https://ae04.alicdn.com/kf/Hfaabfeeff48a463a95c168e0f75788255.jpg",
+          "Уникальные дизайнерские вещи": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhtlWIGzRIYSjkQaN3m_JdMP9rexOSmPgW6uBESWvqR-VyKK3h0UnNzqAKpwf_9WKsmoR-kG3eQH3nvWmjDetLgFUKePZIttQ-i2PgPjSXiSXe8zQL4W6wlvTaDlj44cskV11M90VV6PloECYT8mxTruQGhDqzSZ3D6F6IfsiryQ-luuiUGX1Ig6ZWiww/s640/incredibly-amazing-dresses-by-sylvie-facon-1-5a951a9d355f8__700-480x640.jpg"
+        }
+      }
 }
 
-function getGifts(age, recipient, occasion, interests) {
-  // Здесь можно использовать серверный запрос или статический список подарков
-  // В этом примере я использовал статический список
-  // Можно заменить на реальные данные, используя базу данных или API
+function removeGifts(){
+    const all_gifts = document.querySelectorAll(".gift");
+    all_gifts.forEach((el) => {
+            el.remove()
+    })
+}
 
-  var gifts = [];
+function findGifts() {
+    removeGifts();
 
-  if (age === "0-10") {
-      gifts.push("Игрушка", "Книжка", "Конструктор");
-  } else if (age === "11-20") {
-      gifts.push("Спортивный инвентарь", "Модная одежда", "Гаджет");
-  } else if (age === "21-30") {
-      gifts.push("Книга", "Билеты на концерт", "Дизайнерская одежда");
-  } else if (age === "31-40") {
-      gifts.push("Кухонная утварь", "Спа-подарочный набор", "Аксессуары для отдыха");
-  } else if (age === "41-50") {
-      gifts.push("Ароматическая свеча", "Товары для сада", "Ювелирные украшения");
-  } else if (age === "51+") {
-      gifts.push("Подписка на журнал", "Комфортный халат", "Подарочный сертификат");
-  }
-
-  if (recipient === "Мужчины") {
-      gifts.push("Устройство для барбекю", "Инструменты", "Эксклюзивный карманный нож");
-  } else if (recipient === "Женщины") {
-      gifts.push("Косметический набор", "Украшения", "Спа-услуги");
-  } else if (recipient === "Дети") {
-      gifts.push("Игрушка", "Спортивный инвентарь", "Книжка");
-  } else if (recipient === "Пары") {
-      gifts.push("Романтический ужин", "Совместная поездка", "Подарочный сертификат на спа");
-  } else if (recipient === "Друзья") {
-      gifts.push("Смешные носки", "Игра настольная", "Абонемент в спортзал");
-  }
-
-  if (occasion === "День рождения") {
-      gifts.push("Праздничный торт", "Праздничные шары", "Сюрприз-подарок");
-  } else if (occasion === "Юбилей") {
-      gifts.push("Подарок с символикой юбилея", "Эксклюзивный подарок", "Путешествие");
-  } else if (occasion === "Свадьба") {
-      gifts.push("Сервиз для чая", "Коврик для ванной", "Подарочные деньги");
-  } else if (occasion === "Новый год") {
-      gifts.push("Набор для горячего шоколада", "Новогодние украшения", "Подарочная книга");
-  } else if (occasion === "Другой") {
-      gifts.push("Персонализированный подарок", "Нечто необычное", "Уникальный предмет интерьера");
-  }
-
-  if (interests === "Книги") {
-      gifts.push("Лучший бестселлер года", "Классическая книга", "Аудиокнига");
-  } else if (interests === "Спорт") {
-      gifts.push("Набор для йоги", "Спортивный инвентарь", "Абонемент в фитнес-клуб");
-  } else if (interests === "Технологии") {
-      gifts.push("Умные часы", "Наушники", "Гаджет последней модели");
-  } else if (interests === "Путешествия") {
-      gifts.push("Компактный чемодан", "Путевой набор", "Путеводитель");
-  } else if (interests === "Мода") {
-      gifts.push("Аксессуары", "Модная одежда", "Уникальные дизайнерские вещи");
-  }
-
-  return gifts;
+    const select = document.querySelectorAll("select");
+    let i = 0;
+    for (const el of select) {
+        const gifts = gifts_list[el.id][el.value];
+        for (const gift of Object.keys(gifts)){
+            i += 1;
+            const img_link = gifts[gift];
+            let card = document.createElement('div');
+            card.classList.add('gift');
+            let img = document.createElement('img');
+            img.setAttribute('src', img_link);
+            img.setAttribute('alt', gift);
+            card.appendChild(img);
+            let text = document.createElement('span');
+            text.innerHTML = gift;
+            card.appendChild(text);
+            document.getElementById('giftList').appendChild(card);
+            setInterval(()=> {
+                card.classList.add('open')
+            }, i * 100)
+        }
+    }
 }
